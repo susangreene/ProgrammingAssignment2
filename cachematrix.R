085f894c16bacbeeb777d2de6fd574904db6c3f6
susangreene <57118183+susangreene@users.noreply.github.com>
2020-02-19 20:13:00 -0500

    Updated cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This sets a matrix, uses a cache function within 
+## the matrix, and gets the matrix in order 
+## so that the inverse can be calculated.
+## Cachesolve is then used to calculate the solution to the inverse
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# set the matrix
 
+makecachematrix <- function(z = matrix()) {
+  
+  estab = function(c)  #establish function
+  inv = NULL{          # inverse set to NULL 
+    inv <<- NULL
+    z <<- c
+  }
+  
+  # get the matrix, set the inverse 
+  
+  GETinve = function()inv
+  list(estab=estab, GETinve = GETinve, GET=GET,  
+       estabinve=estabinve)
+}
+  
+  GET = function() z
+  estabinve = function(inv2) inv <<- inv2
+  
+  # retrieve cache info
+
+cachesolve <- function(z, ...){
+  
+  inv <- z$GETinve() 
+   if(!is.null(inv)) {
+    message("retrieving cached information")  
+    return(inv)
+  }
+  
+  output <- z$GET()  # output to be the inverse, solve
+  inv <-solve(output)
+  z$estabinv(inv)
+  inv
 }
 
+# perform rbind, set cachematrix
 
-## Write a short comment describing this function
+z = rbind (c(3,1), c(2,3))
+r = makecachematrix(z)
+r$GET()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# run cachesolve(r) to get solution
+
+cachesolve(r)    # solution 0.4285  -0.1428
+                 #         -0.2857   0.4285
+
+z = rbind(c(2,4), c(5,3))
+r = makecachematrix(z)
+r$GET()
+
+cachesolve(r)    # solution -0.214  0.2857
+                 #           0.357 -0.1428
+
+z = rbind(c(10,4), c(9,7))
+r = makecachematrix(z)
+r$GET()
+
+cachesolve(r)     # solution  0.2058  -0.1176
+                  #          -0.2647   0.2941
+
+
+
+
+
+
